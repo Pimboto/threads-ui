33d19179acb3d61826b29d951e78dc7a62e61d4b
PimboDev <Pimboto@hotmail.com>
2025-03-25 10:52:19 -0500

    se arregla la tipografia

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,23 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type React from "react"
+import "@/app/globals.css"
+import { Mona_Sans as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
 
-// Importa la fuente desde Google Fonts
-import { Poppins } from 'next/font/google'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '700'], // Puedes ajustar los pesos según lo que necesites
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
 })
-
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en" className={poppins.className}>
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>{children}</body>
     </html>
   )
 }
+
